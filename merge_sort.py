c03d7ef978927ff4188a8e59ecd43fd412880176
paulngouchet <ngouchet@bu.edu>
2020-08-01 19:37:53 -0400

    Clean Code

--- merge_sort.py
+++ merge_sort.py
@@ -1,23 +1,15 @@
 # merge sort
 
-
 def merge(nums1, nums2):
-
     result = []
-
     i, j = 0, 0
     while (i < len(nums1) and j < len(nums2)):
-
         if nums1[i] <= nums2[j]:
-
             result.append(nums1[i])
             i += 1
-
         elif nums2[i] <= nums1[i]:
-
             result.append(nums2[j])
             j += 1
-
 
     if i < len(nums1):
         result += nums1[i:]
@@ -28,18 +20,12 @@ def merge(nums1, nums2):
 
 
 def merge_sort(nums):
-
     if len(nums) <= 1:
         return nums
 
     floor = len(nums) // 2
-
     left = merge_sort(nums[:floor])
     right = merge_sort(nums[floor:])
-
-
     return merge(left, right)
-
-
 
 print(merge_sort([1, 5, 6, 8, 3, 0]))
