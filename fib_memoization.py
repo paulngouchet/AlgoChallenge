de2a337c6dfd3dbfe5d190b9b06884b838cf64b8
paulngouchet <ngouchet@bu.edu>
2020-08-01 19:35:08 -0400

    Clean Code

--- fib_memoization.py
+++ fib_memoization.py
@@ -1,21 +1,15 @@
 # Example of dynamic programming
 
-
-
 def fibonacci(n, fib_table):
-
     if n == 1 or n == 2:
         return 1
 
     if n in fib_table:
         return fib_table[n]
-
     else:
         fib_table[n] = fibonacci(n-1, fib_table) + fibonacci(n-2, fib_table)
 
     return fib_table[n]
 
-
 fib_table ={1:1, 2:1}
-
 print(fibonacci(4, fib_table))
