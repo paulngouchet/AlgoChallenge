e287ee2961a64988bb42c7b0cd049fb3cdc2dfbe
paulngouchet <ngouchet@bu.edu>
2020-08-01 19:40:32 -0400

    Clean Code

--- selection_sort.py
+++ selection_sort.py
@@ -1,20 +1,13 @@
 # Implementation of selection sort unsing python
 
 def selection(nums):
-
     for i in range(len(nums)):
-
         min_index = i
-
         for j in range(i+1, len(nums)):
-
             if nums[j] < nums[min_index]:
                 min_index = j
-
         nums[i], nums[min_index] = nums[min_index], nums[i]
 
     return nums
-
-
 
 print(selection([64, 25, 12, 22, 11]))
