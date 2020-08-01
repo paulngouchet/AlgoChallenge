d3aa47110f8519fbbc23eeeca25e29c5aabeaf20
paulngouchet <ngouchet@bu.edu>
2020-08-01 19:36:07 -0400

    Clean Code

--- maxDepth.py
+++ maxDepth.py
@@ -1,13 +1,10 @@
 '''Given binary tree [3,9,20,null,null,15,7],
-
     3
    / \
   9  20
     /  \
    15   7
 return its depth = 3.
-
-
 Method - traverse the tree and at each stage increment the depth variable '''
 
 def maxDepth(self, root):
@@ -15,11 +12,9 @@ def maxDepth(self, root):
         :type root: TreeNode
         :rtype: int
         """
-
         if root == None:
             return 0
-
+        
         leftDepth = self.maxDepth(root.left)
         rightDepth = self.maxDepth(root.right)
-
         return (max(leftDepth, rightDepth) + 1) start at -1 since i will return the depth when i am after the leaf when it null
